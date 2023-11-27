6bb029d420a1721d0bfc52db15d095883e1fbd6d
German Terrile <gterrile@gmail.com>
2023-11-26 22:16:54 -0800

    hourly forecast done. Working on next day forecast

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import './style.css'
 import './reset.css'
 
-const cities = require('all-the-cities');
-
-
 // const icon = document.getElementById('icon');
 // icon.src = '//cdn.weatherapi.com/weather/64x64/night/113.png'
 
@@ -17,7 +14,7 @@ searchButton.addEventListener('click', function() {
 async function getWeather(city) {
   const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=64e203b4a6a54f138eb52839232511&q=${city}`);
   const obj = await response.json();
-  console.log('current', obj);
+  //console.log('current', obj);
   displayWeatherInfo(obj);
 }
 
@@ -26,6 +23,10 @@ function displayWeatherInfo(obj) {
   city.textContent = obj.location.name;
   const country = document.getElementById('text-country');
   country.textContent = obj.location.country;
+
+  const localTime = (obj.location.localtime).split(' ')[1];
+  const time = document.getElementById('text-time');
+  time.textContent = localTime;
   const temp = document.getElementById('text-temp');
   temp.textContent = obj.current.temp_f
   const description = document.getElementById('text-description');
@@ -37,7 +38,57 @@ function displayWeatherInfo(obj) {
 async function getForecast(city) {
   const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=64e203b4a6a54f138eb52839232511&q=${city}&days=3`);
   const obj = await response.json();
-  console.log('forecast', obj);
+  //console.log('forecast', obj);
+  displayHourlyForecast(obj);
+  displayNextDaysForecast(obj);
+}
+
+function displayHourlyForecast(obj) {
+
+  const hours = document.getElementById('hours');
+
+  while(hours.firstChild) {
+    hours.removeChild(hours.firstChild)
+  }
+
+  const localtime = (obj.location.localtime).split(' ')[1];
+  let currentHour = parseInt(localtime.split(':')[0]);
+  let currentday = 0
+  for (let i = 0; i < 5; i++) {
+
+    
+
+    const hour = document.createElement('div');
+    hour.classList.add('hour');
+    hours.appendChild(hour);
+    const time = document.createElement('span');
+    hour.appendChild(time);
+    time.textContent = ((obj.forecast.forecastday[currentday].hour[currentHour].time).split(' ')[1]).split(':')[0];
+    const icon = document.createElement('img');
+    hour.appendChild(icon);
+    icon.src = obj.forecast.forecastday[currentday].hour[currentHour].condition.icon;
+    const temp = document.createElement('span');
+    hour.appendChild(temp);
+    temp.textContent = obj.forecast.forecastday[currentday].hour[currentHour].temp_f + ' °F';
+
+    // console.log(((obj.forecast.forecastday[currentday].hour[currentHour].time).split(' ')[1]).split(':')[0])
+    // console.log(obj.forecast.forecastday[currentday].hour[currentHour].condition.icon)
+    // console.log(obj.forecast.forecastday[currentday].hour[currentHour].temp_f)
+    
+    if (currentHour < 23) {
+      currentHour += 1
+    } else {
+      currentday += 1
+      currentHour = 0
+    }
+
+  }
+}
+
+
+
+function displayNextDaysForecast(obj) {
+
 }
 
 getWeather('Buenos Aires')
